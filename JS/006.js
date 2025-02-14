75797ad7ba67b689b87cf5a2d0e43f4a71330200
Andrew Skow <skovorodnikov11@gmail.com>
2025-02-14 15:34:03 +0300

    js_6_170

---
+++ JS/006.js
@@ -0,0 +1,34 @@
+// how to create variables
+var x;
+let y;
+
+// how to use variables
+x = 5;
+y = 6; 
+let z = x + y;
+
+// синтакиси js определяет два типа значений 
+// 1. фиксированные занчения (литералы)
+// 2. изменяемые значения (переменные)
+
+/*
+
+примеры литерлов 
+
+1. числа 10.50, 1001
+2. строки "John Doe"
+
+*/
+
+// операторы + - * /
+
+my_string = "John" + " " + "Doe";
+console.log(my_string);
+
+// keywords let, var, const
+
+let $myVariable = 12;
+console.log($myVariable);
+
+// дефисы не допустимы в JS
+
